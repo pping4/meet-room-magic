21635d424075a907381ef712612dcd8fdd2c9f5c
pping4 <pping4@yahoo.com>
2025-09-03 10:09:00 +0700

    Update FeaturesSection.tsx
    grid-cols-2

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -36,7 +36,7 @@ const FeaturesSection = () => {
           </p>
         </div>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature, index) => (
             <div 
               key={index} 
